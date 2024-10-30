52ffe2cc93d34eb5987b0c7c6e6e382f59fc3543
ibevers <isaacbevers@gmail.com>
2024-10-30 13:39:04 -0400

    Integrate preview into dashboard

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,4 +1,5 @@
 import requests
+import os
 
 import streamlit as st
 import streamlit.components.v1 as components
@@ -76,13 +77,21 @@ def dataset_metadata_page(tab_name):
     coming_soon_message(tab_name)
     
 def dataset_structure_preview_page(tab_name):
-    st.title(f"{tab_name} - Dataset Structure Preview")
-    response = requests.get(B2AIPREVIEW_GITHUB_RAW_URL)
-    if response.status_code == 200:
-        b2aipreview = response.text
+    st.title(f"{tab_name}")
+
+    # Define the path to the HTML file
+    html_file_path = os.path.join(os.path.dirname(__file__), 'bids-like_structure_preview.html')
+    
+    # Check if the HTML file exists
+    if os.path.exists(html_file_path):
+        # Read the HTML file content
+        with open(html_file_path, 'r', encoding='utf-8') as file:
+            b2aipreview = file.read()
+        
+        # Display the HTML content in a scrollable iframe
         components.html(b2aipreview, height=600, scrolling=True)
     else:
-        st.error(f"Failed to load dataset structure preview. Status code: {response.status_code}")
+        st.error("Failed to load dataset structure preview. The HTML file was not found.")
 
 def dataset_quality_dashboard_page(tab_name):
     coming_soon_message(tab_name)
