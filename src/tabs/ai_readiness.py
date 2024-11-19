654764d4ccb15ef7c09ff05c5f8b500b38817dc1
Alexandros Sigaras <alex@sigaras.com>
2024-11-19 16:52:03 -0500

    B2AI-544

--- src/tabs/ai_readiness.py
+++ src/tabs/ai_readiness.py
@@ -1,5 +1,24 @@
 import streamlit as st
-from tabs.utils import coming_soon_message
+# from tabs.utils import coming_soon_message
+
+import pandas as pd
+import numpy as np
 
 def ai_readiness_page(tab_name):
-    coming_soon_message(tab_name)
+    st.markdown(
+        """
+        For detailed descriptions of each criteria, please read: [https://www.biorxiv.org/content/10.1101/2024.10.23.619844v2](https://www.biorxiv.org/content/10.1101/2024.10.23.619844v2)
+        """
+    )
+
+    # Add Figure
+    image_path = "images/ai-readiness-figure-1.png"
+    st.image(image_path, use_column_width=True)
+
+    # Add Table
+    
+    # df = pd.DataFrame(
+    # np.random.randn(10, 5), columns=("col %d" % i for i in range(5))
+    # )
+
+    # st.table(df)
