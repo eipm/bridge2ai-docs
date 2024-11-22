ab0585b2c6c432df56dfaac1d75cc2dd0483f91f
Alexandros Sigaras <alex@sigaras.com>
2024-11-22 12:00:14 -0500

    typo fix

--- src/dashboard.py
+++ src/dashboard.py
@@ -43,7 +43,7 @@ def create_tabs(tabs_func):
 
 def main():  
     # Current version of the app
-    version = "1.2.0"
+    version = "1.2.1"
     # Map tab names to functions
     # In this dictionary, the key is the tab name and the value is the function that will be called when the tab is selected
     # The function is defined in the respective file
