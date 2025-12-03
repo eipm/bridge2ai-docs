b179f088dd14d7f7f49c82579cc4d7222c48d0f5
Evan Ng <evan.ng@sickkids.ca>
2025-12-03 13:48:48 -0500

    Updated docs

--- src/tabs/data_pre_processing.py
+++ src/tabs/data_pre_processing.py
@@ -71,6 +71,8 @@ def data_pre_processing_page(tab_name):
         1.	A fixed feature format that includes static features extracted from the entire waveform
         2.	A temporal format that varies for each audio file depending on the length of recording.
 
+        The questionnaire features are combined into a single table (phenotype.tsv). This can be used for cohort selection.
+
         **Methods of De-identification for v2.0.0**
         All direct identifiers were removed, as these would reveal the identity of the research participant. These include name, civic address, and social security numbers. Indirect identifiers were removed where these created a significant risk of causing participant re-identification, for example through their combination with other public data available on social media, in government registries, or elsewhere. These include select geographic or demographic identifiers, as well as some information about household composition or cultural identity. Non-identifying elements of data that revealed highly sensitive information, such as information about household income, mental health status, traumatic life experiences, and the like were also removed.
 
